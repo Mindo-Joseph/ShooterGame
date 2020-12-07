2cf5d205ce04cf408bc74bd31999dc117c66182d
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-12-07 19:29:58 +0300

    Implement the boot and preloader scenes

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import Phaser from 'phaser';
+import BootScene from './scenes/BootScene';
+import PreloaderScene from './scenes/PreloaderScene';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 960,
+  height: 540,
+  backgroundColor: 'black',
+  autoCenter: Phaser.Scale.CENTER_BOTH,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        x: 0,
+        y: 0,
+      },
+    },
+  },
+  scene: [],
+  pixelArt: true,
+  roundPixels: true,
+};
+
+class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+    this.scene.add('BootScene', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.start('BootScene');
+  }
+}
+
+window.game = new Game();
---
+++ src/scenes/BootScene.js
@@ -0,0 +1,20 @@
+import Phaser, { Renderer } from 'phaser';
+
+export default class BootScene extends Phaser.Scene {
+  constructor() {
+    super('BootScene');
+  }
+
+  preload() {
+    this.load.image('logo', 'assets/logo.png');
+  }
+
+  create() {
+    this.add.image(480, 270, 'logo');
+    this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
+  }
+
+  ready() {
+    this.scene.start('Preloader');
+  }
+}
