d5d4a116b88aee8b10d4f131731fde34d4ff45d8
Restyled.io <commits@restyled.io>
2020-12-16 05:37:52 +0000

    Restyled by clang-format

--- src/helpers/generatename.js
+++ src/helpers/generatename.js
@@ -1,8 +1,5 @@
 const generateName = () => {
-  const name = Math.random()
-    .toString(36)
-    .replace(/[^a-z]+/g, "")
-    .substr(0, 5);
+  const name = Math.random().toString(36).replace(/[^a-z]+/g, "").substr(0, 5);
   return name;
 };
 
--- src/leaderboard/leaderboard.js
+++ src/leaderboard/leaderboard.js
@@ -1,19 +1,19 @@
 /* eslint-disable no-unused-vars */
 import regeneratorRuntime from "regenerator-runtime";
-import { SAVED_NAME } from "../scenes/BootScene";
+import {SAVED_NAME} from "../scenes/BootScene";
 
 const baseUrl =
-  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/";
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/";
 
 const creategame = async () => {
   try {
     const config = {
-      name: SAVED_NAME,
+      name : SAVED_NAME,
     };
     const response = await fetch(`${baseUrl}games/`, {
-      method: "POST",
-      body: JSON.stringify(config),
-      headers: { "Content-type": "application/json; charset=UTF-8" },
+      method : "POST",
+      body : JSON.stringify(config),
+      headers : {"Content-type" : "application/json; charset=UTF-8"},
     });
     let data = await response.json();
     data = JSON.stringify(data);
@@ -35,16 +35,14 @@ const setScore = async (object) => {
 
     const proxyUrl = "https://mysterious-ridge-02468.herokuapp.com/";
 
-    const response = await fetch(
-      `${proxyUrl}${baseUrl}games/${hashedGameName[0]}/scores`,
-      {
-        method: "POST",
-        body: JSON.stringify(object),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      }
-    );
+    const response =
+        await fetch(`${proxyUrl}${baseUrl}games/${hashedGameName[0]}/scores`, {
+          method : "POST",
+          body : JSON.stringify(object),
+          headers : {
+            "Content-type" : "application/json; charset=UTF-8",
+          },
+        });
     const data = await response.json();
     return data;
   } catch (error) {
