aa3c540a98c1d246f53a6c057591ed4d838e8348
Restyled.io <commits@restyled.io>
2020-12-16 05:35:48 +0000

    Restyled by clang-format

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 import Phaser from 'phaser';
 
 import BootScene from './scenes/BootScene';
+import GameOverScene from './scenes/GameOverScene';
+import GameScene from './scenes/GameScene';
 import PreloaderScene from './scenes/PreloaderScene';
 import TitleScene from './scenes/TitleScene';
-import GameScene from './scenes/GameScene';
-import GameOverScene from './scenes/GameOverScene';
 
 const config = {
-  type: Phaser.AUTO,
-  mode: Phaser.Scale.FIT,
-  width: 960,
-  height: 540,
-  dom: {
-    createContainer: true,
+  type : Phaser.AUTO,
+  mode : Phaser.Scale.FIT,
+  width : 960,
+  height : 540,
+  dom : {
+    createContainer : true,
   },
-  backgroundColor: '#56ac68',
-  autoCenter: Phaser.Scale.CENTER_BOTH,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: {
-        x: 0,
-        y: 0,
+  backgroundColor : '#56ac68',
+  autoCenter : Phaser.Scale.CENTER_BOTH,
+  physics : {
+    default : 'arcade',
+    arcade : {
+      gravity : {
+        x : 0,
+        y : 0,
       },
-      debug: false,
+      debug : false,
     },
   },
-  input: {
-    activePointers: 3,
+  input : {
+    activePointers : 3,
   },
-  scene: [],
-  pixelArt: true,
-  roundPixels: true,
+  scene : [],
+  pixelArt : true,
+  roundPixels : true,
 };
 
 class Game extends Phaser.Game {
--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -2,24 +2,18 @@ import Phaser from 'phaser';
 import generateName from '../helpers/generatename';
 
 export default class BootScene extends Phaser.Scene {
-  constructor() {
-    super('BootScene');
-  }
+  constructor() { super('BootScene'); }
 
-  preload() {
-    this.load.image('logo', 'assets/logo.png');
-  }
+  preload() { this.load.image('logo', 'assets/logo.png'); }
 
   create() {
     this.add.image(480, 270, 'logo');
     this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
   }
 
-  ready() {
-    this.scene.start('Preloader');
-  }
+  ready() { this.scene.start('Preloader'); }
 }
 const SAVED_NAME = generateName();
 const playerName = generateName();
 
-export { SAVED_NAME, playerName };
+export {SAVED_NAME, playerName};
