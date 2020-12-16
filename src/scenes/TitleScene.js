6212a01507db1d9036bcfa32eafce2ca48cb4129
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-12-16 19:55:37 +0300

    Change the title scene image

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -6,14 +6,7 @@ export default class TitleScene extends Phaser.Scene {
   }
 
   create() {
-    this.message = this.add
-      .text(400, 100, 'Hello, --', {
-        color: '#FFFFFF',
-        fontSize: 60,
-        fontStyle: 'bold',
-      })
-      .setOrigin(0.5);
-    const button = this.add.image(200, 270, 'startbutton').setScale(0.25);
+    const button = this.add.image(450, 270, 'startbutton').setScale(1.25);
     button.setInteractive();
     const self = this;
     this.input.on('gameobjectdown', () => {
