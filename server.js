0314cabb66f4513b8d9a66c1dc80878866d16a02
Joseph Mindo <mindo.joseph.mj@gmail.com>
2021-01-06 20:21:26 +0300

    Add link to leaderboard in server

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const express = require('express');
 const app = express();
 const DIST_DIR = path.join(__dirname, '/dist');
 const HTML_FILE = path.join(__dirname, 'index.html');
-
+const LEADERBOARD_FILE = path.join(__dirname,'/src/leaderboard/leaderboard.html')
 app.use(express.static(DIST_DIR));
 app.get('/', (req, res) => {
   res.sendFile(HTML_FILE);
 });
-
+app.get('/src/leaderboard/leaderboard.html', (req, res) => {
+  res.sendFile(LEADERBOARD_FILE);
+})
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
