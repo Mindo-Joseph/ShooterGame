000e8f9e79dedddaceb5dbf04f6727c13b15e584
Joseph Mindo <mindo.joseph.mj@gmail.com>
2021-01-06 19:35:28 +0300

    Fix game config for localhost

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const express = require('express');
 
 const app = express();
 const DIST_DIR = path.join(__dirname, '/dist');
-const HTML_FILE = path.join(DIST_DIR, 'index.html');
-
 app.use(express.static(DIST_DIR));
-app.get('*', (req, res) => {
-  res.sendFile(HTML_FILE);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+
+app.listen(8080, () => {
 
 });
