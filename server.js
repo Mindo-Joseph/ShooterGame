30cd69eefb744a3faa33dcc5c92ab54b371d568a
Joseph Mindo <mindo.joseph.mj@gmail.com>
2021-01-06 21:53:05 +0300

    Add static files for use in Heroku

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const express = require('express');
 
 const app = express();
 const DIST_DIR = path.join(__dirname, '/dist');
+const LEADERBOARD_HELPERS = path.join(__dirname, '/src/helpers');
+const LEADERBOARD_FILES = path.join(__dirname,'/src/leaderboard')
 const HTML_FILE = path.join(__dirname, 'index.html');
-const LEADERBOARD_FILE = path.join(__dirname,'/src/leaderboard/leaderboard.html')
+const LEADERBOARD = path.join(__dirname,'/src/leaderboard/leaderboard.html')
 app.use(express.static(DIST_DIR));
+app.use(express.static(LEADERBOARD_FILES));
+app.use(express.static(LEADERBOARD_HELPERS));
 app.get('/', (req, res) => {
   res.sendFile(HTML_FILE);
 });
 app.get('/src/leaderboard/leaderboard.html', (req, res) => {
-  res.sendFile(LEADERBOARD_FILE);
+  res.sendFile(LEADERBOARD);
 })
 const PORT = process.env.PORT || 8080;
 
