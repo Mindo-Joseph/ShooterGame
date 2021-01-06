87981f7990c1fbfcb0308a2695fb204544d922ef
Joseph Mindo <mindo.joseph.mj@gmail.com>
2021-01-06 19:44:33 +0300

    Remove fixed port number for heroku deployment

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require('express');
 
 const app = express();
 const DIST_DIR = path.join(__dirname, '/dist');
+const HTML_FILE = path.join(__dirname, 'index.html');
+
 app.use(express.static(DIST_DIR));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(HTML_FILE);
 });
 
+const PORT = process.env.PORT || 8080;
 
-
-app.listen(8080, () => {
+app.listen(PORT, () => {
 
 });
