819f9ab1f06000ebcfc253e2ebc96ca330091428
Joseph Mindo <mindo.joseph.mj@gmail.com>
2021-01-07 16:53:41 +0300

    Render Leaderboard in new tab on browser

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const DIST_DIR = path.join(__dirname, '/dist');
 const LEADERBOARD_HELPERS = path.join(__dirname, '/src/helpers');
-const LEADERBOARD_FILES = path.join(__dirname,'/src/leaderboard')
+const LEADERBOARD_FILES = path.join(__dirname, '/src/leaderboard');
 const HTML_FILE = path.join(__dirname, 'index.html');
 const LEADERBOARD = path.join(__dirname,'/src/leaderboard/leaderboard.html')
 app.use(express.static(DIST_DIR));
@@ -13,6 +13,9 @@ app.use(express.static(LEADERBOARD_HELPERS));
 app.get('/', (req, res) => {
   res.sendFile(HTML_FILE);
 });
+app.get('/dist/leaderindex.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/leaderindex.js'));
+})
 app.get('/src/leaderboard/leaderboard.html', (req, res) => {
   res.sendFile(LEADERBOARD);
 })
