12f501975bf6beaf9528b6d849cd7188fdafc86b
Joseph Mindo <mindo.joseph.mj@gmail.com>
2021-01-08 23:03:35 +0300

    Add blank line between class members

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -182,7 +182,8 @@ export default class GameScene extends Phaser.Scene {
       this.player.setVelocityY(0);
     }
   }
-  /*eslint class-methods-use-this: ["error", { "exceptMethods": ["hitEnemy"] }] */
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["hitEnemy"] }] */
+  
   hitEnemy(bullet, enemy) {
     bullet.destroy();
     enemy.destroy();
